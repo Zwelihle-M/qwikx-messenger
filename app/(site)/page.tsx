8eb4dc74d83b6875ed81d20e828f362d7dc34bfd
Zwelihle <zwelihle408@gmail.com>
2023-07-13 12:23:49 +0200

    sign page/authentication/middleware complete

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,16 @@
-
-
+import Image from "next/image";
+import QwikxLogo from "../../public/images/qwikx-message-white.png"
+import AuthForm from "./components/AuthForm";
 export default function Home() {
-    return (
-      <div className="flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8 bg-graySeven">
-
+  return (
+    <div className="flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8 bg-graySeven">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">{/* qwikx logo */}
+      <Image src={QwikxLogo}  alt="Logo" height={200} width={300} className="mx-auto w-auto" quality={100} priority/>
+      <h1 className="mt-6 text-center text-3xl tracking-tight text-white">Sign in to your account</h1>
       </div>
-    )
-  }
+
+      {/* authentication form */}
+      <AuthForm/>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,34 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
-
+import "./globals.css";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import ToasterContext from "./context/ToasterContext";
+import AuthContext from "./context/AuthContext";
 
 const poppins = Poppins({
   subsets: ["latin"],
   variable: "--font-poppin",
-  weight: ["100","200","300","400","500","600","700","800","900"]
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
-
 export const metadata: Metadata = {
-  title: 'Qwikx Messenger',
-  description: 'Qwikx Messenger',
-}
+  title: "Qwikx Messenger",
+  description: "Qwikx Messenger",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} font-poppin`}>{children}</body>
+      <body className={`${poppins.variable} font-poppin`}>
+        <AuthContext>
+        <ToasterContext />
+        {children}
+        </AuthContext>
+       
+      </body>
     </html>
-  )
+  );
 }
