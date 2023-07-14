683e1b7d3e23f179e4b8890e5cbac92ef00b7985
Zwelihle <zwelihle408@gmail.com>
2023-07-14 22:21:29 +0200

    conversation box done

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const EmptyState = () => {
   return (
-    <div className='px-4 py-10 sm-px-6 lg:px-8 h-full flex justify-center items-center bg-black/10'>
+    <div className='px-4 py-10 sm-px-6 lg:px-8 h-full flex justify-center items-center bg-graySeven'>
         <div className='text-center items-center flex flex-col'>
-            <h2 className='mt-2 text-3xl'>Select a chat or start a new converstation</h2>
+            <h2 className='mt-2 text-3xl text-white'>Select a chat or start a new converstation</h2>
 
         </div>
     </div>
